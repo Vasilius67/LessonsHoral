17914c07d013908c1218fc1c14a47d409e140acf
Vasilius67 <59847476+Vasilius67@users.noreply.github.com>
2020-01-22 20:55:59 +0200

    1

--- 3/3.js
+++ 3/3.js
@@ -1,1 +1,19 @@
-start
+//3.1
+//a
+var arr = [1,2,3,4,5];
+var sum1 = 1;
+for (var i = 0; i < arr.length;i++) {sum1= arr[i]*sum1}
+  console.log(sum1);
+//b
+let arr = [1,2,3,4,5];
+var sum1 = 1;
+let i = 0;
+while (i < arr.length)  {
+  sum1 = arr[i]*sum1 
+ i++
+ };
+  console.log(sum1);
+  //3.2
+  
+  
+
