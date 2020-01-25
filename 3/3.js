7f912aef6492fe92aa8e99ee05dcfcb5c20d863a
Vasilius67 <59847476+Vasilius67@users.noreply.github.com>
2020-01-25 10:39:58 +0200

    ended2

--- 3/3.js
+++ 3/3.js
@@ -51,7 +51,7 @@ function uppercase(str)
 {
   let  array1 = str.split(' ');
   let newarray1 = [];
-  for(let i = 0; x < array1.length; i++){
+  for(let i = 0; i < array1.length; i++){
       newarray1.push(array1[i].charAt(0).toUpperCase()+array1[i].slice(1));
   }
   return newarray1.join(' ');
